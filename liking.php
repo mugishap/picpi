10d98e8929a8cf46e463eb6bbf5b3d65ff1295c2
mugishap <precieuxmugisha@gmail.com>
2022-05-27 10:34:38 +0200

    finishing up some likes

--- liking.php
+++ liking.php
@@ -1,3 +1,15 @@
 <?php
+$like = mysqli_query( $connection, "INSERT INTO likes(liker_id,post_id,liker_profile,likerusername) VALUES('$userid','','$profile','$username')" );
+if ( !$like ) {
+    ?>
+    e.classList.replace( 'bxs-like', 'bx-like' )
+    <?php
+}
+$dislike = mysqli_query( $connection, "DELETE FROM likes WHERE liker_id='$userid')" );
+if ( !$dislike ) {
+    ?>
+    e.classList.replace( 'bxs-like', 'bx-like' )
+    <?php
+}
 
 ?>
